5678081bac1267daa935c3324179a3a3f063dd99
jordanbrennan <jordanbrennan@icloud.com>
2024-06-16 13:26:34 -0500

    removes old docs and gulp

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 const atImport = require("postcss-import");
 const autoprefixer = require('autoprefixer');
-const customProperties = require('postcss-custom-properties');
 
 module.exports = {
   plugins: [
     atImport,
     autoprefixer,
-    customProperties({preserve: false}),
   ]
 };
